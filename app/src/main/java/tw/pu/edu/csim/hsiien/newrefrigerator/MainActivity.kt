c0577317d677979224ef81b9ba322819af1c51f9
cindili1215 <cindy1050286@gmail.com>
2025-03-03 14:59:46 +0800

    v0 cindi test

--- app/src/main/java/tw/pu/edu/csim/hsiien/newrefrigerator/MainActivity.kt
+++ app/src/main/java/tw/pu/edu/csim/hsiien/newrefrigerator/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello123 嫺桑!",
+        text = "Hello123 嫺桑! abc",
         modifier = modifier
     )
 }

3760a849bcb184275e7e6a459558c418763c1dff
test <jenny1211jenny@gmail.com>
2025-03-03 14:23:26 +0800

    v2jtest

--- app/src/main/java/tw/pu/edu/csim/hsiien/newrefrigerator/MainActivity.kt
+++ app/src/main/java/tw/pu/edu/csim/hsiien/newrefrigerator/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello 嫺!",
+        text = "Hello 嫺嫺桑!",
         modifier = modifier
     )
 }
